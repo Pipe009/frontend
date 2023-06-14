90edc6490875f2ef51f815811015960eb6a62d51
009 Nattawut Piyachan <ji560chan@gmail.com>
2023-06-14 10:05:03 +0700

    Updata

--- pages/index.js
+++ pages/index.js
@@ -3,52 +3,71 @@ import Head from 'next/head'
 import Image from 'next/image';
 import 'bootstrap/dist/css/bootstrap.css';
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"></link>
-
 export default function index() {
   return(
     <>
+
+
 
     <Head>
       <title>welcome to Jichan</title>
     </Head>
 
-    <div class="text-center">
-  <img src="https://media.discordapp.net/attachments/851074849631895552/1106884326550933514/71418124-69D9-44ED-A8FB-B8BE1E33AB73.gif" className="rounded" alt="Xd" width={200} height={200}/>
-  <button type="button" class="btn btn-link">JiChan</button>
-</div>
+    <header data-bs-theme="light">
+  <nav className="navbar navbar-expand-md navbar-light fixed-top bg-light">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">JiChan.</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarCollapse">
+        <ul className="navbar-nav me-auto mb-2 mb-md-0">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page"Link href="/.">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link"Link href="information">information</a>
+          </li>
+          <li className="nav-item dropdown">
+</li>
 
-<div class="carousel-inner">
-      <div class="carousel-item carousel-item-next carousel-item-start">
-        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
-        <div class="container">
-          <div class="carousel-caption text-start">
-            <h1>Example headline.</h1>
-            <p class="opacity-75">Some representative placeholder content for the first slide of the carousel.</p>
-            <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
-          </div>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
-        <div class="container">
-          <div class="carousel-caption">
-            <h1>Another example headline.</h1>
-            <p>Some representative placeholder content for the second slide of the carousel.</p>
-            <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
-          </div>
-        </div>
-      </div>
-      <div class="carousel-item active carousel-item-start">
-        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
-        <div class="container">
-          <div class="carousel-caption text-end">
-            <h1>One more for good measure.</h1>
-            <p>Some representative placeholder content for the third slide of this carousel.</p>
-            <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
-          </div>
-        </div>
+          <li className="nav-item">
+            <a className="nav-link "Link href="price">price</a>
+          </li>
+        </ul>
+        <form className="d-flex" role="search">
+          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button className="btn btn-outline-success" type="submit">Search</button>
+        </form>
       </div>
     </div>
+  </nav>
+</header>
+
+<main>
+<div id="carouselExample" className="carousel slide">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <Image src="/000.jpg" className="d-block w-100" alt="view" width={400} height={800}/>
+    </div>
+    <div className="carousel-item">
+      <Image src="/001.jpg" className="d-block w-100" alt="view2" width={400} height={800}/>
+    </div>
+    <div className="carousel-item">
+      <Image src="/002.jpg" className="d-block w-100" alt="view3" width={400} height={800}/>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true" />
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true" />
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+</main>
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     </>
