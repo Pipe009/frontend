f7c53d02859e125d9a07b07e9c365a4f31c3340e
009 Nattawut Piyachan <ji560chan@gmail.com>
2023-09-07 22:32:46 +0700

    S

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,7 +1,5 @@
-// get the client
 const mysql = require("mysql2");
 
-// create the connection to database
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
@@ -11,54 +9,70 @@ const connection = mysql.createConnection({
 });
 
 export default function handler(req, res) {
+
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*') // replace this your actual origin
+  res.setHeader('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  
+  /*------------------------------------------------------------------*/
   const { studentid, firstname, lastname, username, password, status } = req.body;
-
   if (req.method === "GET") {
-
     try {
-      connection.query("SELECT * FROM `tbl_users`", function (err, results) {
-        res.status(200).json({ users: results });
-      });
+      connection.query(
+        "SELECT * FROM `tbl_users`",
+        function (err, results, fields) {
+          res.status(200).json({ users: results });
+        }
+      );
     } catch (error) {
       return res.status(500).json({ message: error.message });
     }
-
+    /*------------------------------------------------------------------*/
   } else if (req.method === "POST") {
-
     try {
-      const result = connection.query("INSERT INTO tbl_users SET ?", {
+      const results = connection.query("INSERT INTO tbl_users SET ?", {
         studentid,
         firstname,
         lastname,
         username,
         password,
-        status
+        status,
       });
-      return res.status(200).json({ ...req.body, id: result.insertId });
+      return res.status(200).json({ "status": "ok", "message":req.body, id: results.insertId });
     } catch (error) {
       return res.status(500).json({ message: error.message });
     }
+    /*------------------------------------------------------------------*/
+  } else if (req.method === 'PUT') {
 
-  } else if (req.method === "PUT") {
+    try{
+    const result = connection.query("UPDATE tbl_users SET ? WHERE id = ?",[
+      req.body,
+      req.body.id,
+    ]);
 
-    try {
-      const result = connection.query("UPDATE tbl_users SET ? WHERE id = ?", [
-        req.body,
-        req.body.id,
-      ]);
-      return res.status(200).json({ ...req.body, id: result.insertId });
-    } catch (error) {
-      return res.status(500).json({ message: error.message });
-    }
+    return res.status(200).json({ "status": "ok", "message" :req.body, id: result.insertId});
 
-  } else {
-   
-    try {
-      const result = connection.query("DELETE FROM tbl_users WHERE id = ?", [req.query.id]);
-      return res.status(200).json({ ...req.body, id: result.insertId });
-    } catch (error) {
-      return res.status(500).json({ message: error.message });
-    }
-
+  } catch (error){
+    return res.status(500).json({ message: error.message });
   }
+
+
+  } else{
+
+    try{
+      const result = connection.query("DELETE FROM tbl_users WHERE id = ?",[req.query.id]);
+  
+      return res.status(200).json({ ...req.body, id: result.insertId});
+  
+    } catch (error){
+      return res.status(500).json({ message: error.message });
+    }
+    
+  }
+
 }
